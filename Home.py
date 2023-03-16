428db5869041f66837a46038568e24f3ef40c271
bouchra zidaoui <bouchra.zid@gmail.com>
2023-03-16 11:36:26 +0100

    frontend update

--- Home.py
+++ Home.py
@@ -1,8 +1,28 @@
 import streamlit as st
+from PIL import Image
+import streamlit.components.v1 as components
+
+#styling
+# def get_style():
+#     with open("style.css") as f:
+#         st.markdown("""<link rel="stylesheet" type="text/css" href="style.css">""", unsafe_allow_html=True)
+# get_style()
+
+
+st.markdown("""<style>
+            .css-10trblm.e16nr0p30{
+                    color: lightblue;
+            }
+
+            </style>""", unsafe_allow_html=True)
+
+#logo reading
+logo = Image.open('/home/bushra/code/Loupy2023/get_it_right_frontend/assets/logo.png')
+st.image(logo,width=200, use_column_width=None)
 
 def main():
     st.title("Home Page")
     st.write("Welcome on board!")
 
-if __name__ == '__main__': #check if this line is needed !
+if __name__ == '__main__':
     main()
